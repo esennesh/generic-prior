5d6d38d3f1bdff65b991ea67c2c1b05c0e69e3f9
Eli Sennesh <esennesh@ccis.neu.edu>
2018-12-19 12:29:55 -0500

    Control.Monad.Bayes.Prior: define GMonadPrior over basic representations
    Signed-off-by: Eli Sennesh <esennesh@ccis.neu.edu>

--- src/Control/Monad/Bayes/Prior.hs
+++ src/Control/Monad/Bayes/Prior.hs
@@ -9,19 +9,61 @@ Portability : GHC
 -}
 
 module Control.Monad.Bayes.Prior (
-  MonadPrior,
-  prior
+  GMonadPrior,
+  GPriorScore
+  --prior,
+  --priorProbability,
 ) where
 
 import Control.Monad.Bayes.Class
 import GHC.Generics
 import Numeric.Log
 
-class PriorScore a where
-  priorProbability :: a -> Log Double
+class GMonadPrior f where
+  gPrior :: (MonadSample m) => m (f p)
 
-class MonadSample m => MonadPrior m a where
-  prior :: m a
+class GPriorScore f where
+  gPriorProbability :: f p -> Log Double
 
-priorScore :: (MonadInfer m, MonadPrior m a, PriorScore a) => a -> m ()
-priorScore = score . priorProbability
+instance GMonadPrior V1 where
+  gPrior = error "Cannot sample from the empty sum-type!"
+
+instance GPriorScore V1 where
+  gPriorProbability _ = error "Cannot score under the empty sum-type!"
+
+instance GMonadPrior U1 where
+  gPrior = return U1
+
+instance GPriorScore U1 where
+  gPriorProbability U1 = 1
+
+instance (GMonadPrior a, GMonadPrior b) => GMonadPrior (a :*: b) where
+  gPrior = do
+    a <- gPrior
+    b <- gPrior
+    return (a :*: b)
+
+instance (GPriorScore a, GPriorScore b) => GPriorScore (a :*: b) where
+  gPriorProbability (a :*: b) = gPriorProbability a * gPriorProbability b
+
+class GMonadPriorSum f where
+  gPriors :: MonadSample m => [m (f p)]
+
+instance GMonadPriorSum V1 where
+  gPriors = [gPrior]
+
+instance GMonadPriorSum U1 where
+  gPriors = [gPrior]
+
+instance (GMonadPrior a, GMonadPrior b) => GMonadPriorSum (a :*: b) where
+  gPriors = [gPrior]
+
+instance (GMonadPriorSum a, GMonadPriorSum b) => GMonadPriorSum (a :+: b) where
+  gPriors = (map (L1 <$>) as) ++ (map (R1 <$>) bs) where
+    as = gPriors
+    bs = gPriors
+
+instance (GMonadPriorSum a, GMonadPriorSum b) => GMonadPrior (a :+: b) where
+  gPrior = let priors = gPriors in do
+    i <- uniformD [0..length priors - 1]
+    priors !! i
