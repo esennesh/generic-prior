f92c56f3ae9413c888bffbead0b443516f3f54b4
Eli Sennesh <esennesh@ccis.neu.edu>
2020-02-26 23:35:29 -0500

    Control.Monad.Bayes.Partial: use unification to score partial structures
    Signed-off-by: Eli Sennesh <esennesh@ccis.neu.edu>

---
+++ src/Control/Monad/Bayes/Partial.hs
@@ -0,0 +1,54 @@
+{-|
+Module      : Control.Monad.Bayes.Partial
+Description : Distributions over partially structured data of arbitrary types
+Copyright   : (c) Eli Sennesh, 2020
+License     : MIT
+Maintainer  : esennesh@ccis.neu.edu
+Stability   : experimental
+Portability : GHC
+-}
+
+module Control.Monad.Bayes.Partial where
+
+import Control.Monad.Bayes.Class
+import Control.Monad.Bayes.Prior
+import Control.Monad.Identity
+import Control.Monad.Trans
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Except
+import Control.Unification
+import Control.Unification.IntVar
+import Control.Unification.Types
+import Data.Functor.Fixedpoint
+import Data.Maybe
+import GHC.Generics
+import Numeric.Log
+
+instance PriorScore1 f => PriorScore (Fix f) where
+  priorProbability (Fix f) = priorProbability1 f
+
+type Failure f      = UFailure f IntVar
+type BindingState f = IntBindingState f
+type Binder f a     = (IntBindingT f Identity) (Either (Failure f) a)
+type UExcept f      = Either (Failure f)
+
+evalFBM :: Binder f a -> UExcept f a
+evalFBM = runIdentity . evalIntBindingT
+
+unifyDensity :: (Unifiable f, PriorScore1 f) => UTerm f IntVar -> Fix f ->
+                                                Log Double
+unifyDensity template token = case binding of
+  Left _ -> 0
+  Right fs -> product $ map (maybe 0 priorProbability) fs
+  where
+    binding = evalFBM $ unifiers template (unfreeze token)
+
+unifiers :: Unifiable f => UTerm f IntVar -> UTerm f IntVar ->
+            (IntBindingT f Identity) (Either (Failure f) [Maybe (Fix f)])
+unifiers x y = do
+  x' <- runExceptT $ x =:= y
+  case x' of
+    Left failure   -> return $ Left failure
+    Right solution -> let vars = foldr (\v vs -> v:vs) [] solution in do
+      vals <- mapM lookupVar $ vars
+      return . Right $ [join (freeze <$> val) | val <- vals]
